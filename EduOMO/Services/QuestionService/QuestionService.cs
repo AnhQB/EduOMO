8bc7c886653684f8088c5cf42160cc229c4bab15
Anh Phạm <phamanhqb73@gmail.com>
2025-06-14 22:58:39 +0700

    add question service

--- EduOMO/Services/QuestionService/QuestionService.cs
+++ EduOMO/Services/QuestionService/QuestionService.cs
@@ -1,6 +1,6 @@
-﻿using EduOMO.Data.Base;
+﻿using EduOMO.Base;
+using EduOMO.Data.Base;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
 using MMOEdu.Data;
 
 namespace EduOMO.Services;
@@ -23,6 +23,11 @@ public class QuestionService : IQuestionService
         return await _context.Question.AsNoTracking().Where(x => !x.IsDeleted).OrderByDescending(x => x.CreatedAt).Take(10).ToListAsync();
     }
 
+    public async Task<IEnumerable<QuestionEntity>> GetFirst5QuestionsNotCurrent(Guid id)
+    {
+        return await _context.Question.AsNoTracking().Where(x => !x.IsDeleted && id != x.Id).OrderByDescending(x => x.CreatedAt).Take(5).ToListAsync();
+    }
+
     public async Task<QuestionEntity> GetQuestionById(Guid id)
     {
         return await _context.Question.FirstOrDefaultAsync(p => p.Id == id) ?? new QuestionEntity();
@@ -31,6 +36,22 @@ public class QuestionService : IQuestionService
     public async Task<QuestionEntity> GetQuestionBySlug(string slug)
     {
         return await _context.Question.FirstOrDefaultAsync(p => p.Slug == slug && !p.IsDeleted) ?? new QuestionEntity();
+    }
+
+    public async Task AddQuestion(QuestionEntity question)
+    {
+        var slug = GenerateSlugHelper.GenerateSlug(question.Content);
+        question.Slug = slug;
+        _context.Question.Add(question);
+        await _context.SaveChangesAsync();
+    }
+    public async Task SoftDeleteQuestion(Guid id)
+    {
+        var question = await _context.Question.FirstOrDefaultAsync(x => x.Id == id && !x.IsDeleted);
+        if (question == null) return;
+        question.IsDeleted = true;
+        _context.Question.Update(question);
+        await _context.SaveChangesAsync();
     }
 
     public async Task UpdateQuestion(QuestionEntity question)
