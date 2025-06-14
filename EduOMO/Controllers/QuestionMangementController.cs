d86657975abe495204fdae4c6b27e6f22cd6cb5a
Anh Phạm <phamanhqb73@gmail.com>
2025-06-14 22:58:39 +0700

    add question service

---
+++ EduOMO/Controllers/QuestionMangementController.cs
@@ -0,0 +1,15 @@
+﻿using EduOMO.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduOMO.Controllers
+{
+    public class QuestionMangementController : Controller
+    {
+        private readonly IQuestionService questionService;
+
+        public QuestionMangementController(IQuestionService questionService)
+        {
+            this.questionService = questionService;
+        }
+    }
+}
--- EduOMO/Services/QuestionService/IQuestionService.cs
+++ EduOMO/Services/QuestionService/IQuestionService.cs
@@ -6,7 +6,10 @@ public interface IQuestionService
 {
     Task<IEnumerable<QuestionEntity>> GetAllQuestions();
     Task<IEnumerable<QuestionEntity>> GetFirst10Questions();
+    Task<IEnumerable<QuestionEntity>> GetFirst5QuestionsNotCurrent(Guid id);
     Task<QuestionEntity> GetQuestionById(Guid id);
     Task<QuestionEntity> GetQuestionBySlug(string slug);
+    Task AddQuestion(QuestionEntity question);
     Task UpdateQuestion(QuestionEntity question);
+    Task SoftDeleteQuestion(Guid id);
 }
